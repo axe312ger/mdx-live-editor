9bcda0e133fd5acccb5f299bad4d0577b60d6cff
Benedikt Rötsch <info@benedikt-roetsch.de>
2019-04-07 22:49:58 +0800

    Updates

---
+++ precache-manifest.b5912de3de6194dff2cc48a7ed510609.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "86afd20cd9f1a537afbb",
+    "url": "/static/js/main.b5dae5d9.chunk.js"
+  },
+  {
+    "revision": "8648f7e7c1ac9577905a",
+    "url": "/static/js/2.a4f80e5c.chunk.js"
+  },
+  {
+    "revision": "86afd20cd9f1a537afbb",
+    "url": "/static/css/main.4218f649.chunk.css"
+  },
+  {
+    "revision": "8648f7e7c1ac9577905a",
+    "url": "/static/css/2.a4fb334a.chunk.css"
+  },
+  {
+    "revision": "d6ea5c1db6469fd87107125ef6808d1f",
+    "url": "/index.html"
+  }
+];
