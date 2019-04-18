19ea0db676ad16f034cdb8d792107e499bc4a609
Benedikt Rötsch <info@benedikt-roetsch.de>
2019-04-18 18:57:30 +0700

    fix(demo): improve styling while loading

--- packages/demo/src/App.js
+++ packages/demo/src/App.js
@@ -1,10 +1,16 @@
 import React, { useEffect } from 'react'
 
 import editor from '@mdx-live/editor'
+import styled from 'styled-components'
 import '@axe312/easymde/dist/easymde.min.css'
 
 import Grid from './mdx/Grid'
 import defaultMarkdownValue from './default.md'
+
+const Textarea = styled.textarea`
+  min-width: 100vw;
+  min-height: 100vh;
+`
 
 const components = [
   {
@@ -36,9 +42,5 @@ export default function App() {
     easymde = editor({ components, replacements })
   }, [easymde])
 
-  return (
-    <div id="app">
-      <textarea id="editor" defaultValue={defaultMarkdownValue} />
-    </div>
-  )
+  return <Textarea id="editor" defaultValue={defaultMarkdownValue} />
 }
